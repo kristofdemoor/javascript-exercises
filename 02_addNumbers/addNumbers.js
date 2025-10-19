9f5cf43f29eb95c3518f7216ea297f2b4ef3e3e6
Kristof De Moor <kristof.demoor@protonmail.com>
2025-10-19 18:02:26 +0200

    Complete exercise and Jest Test passed

--- 02_addNumbers/addNumbers.js
+++ 02_addNumbers/addNumbers.js
@@ -4,7 +4,7 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // < Changed strings "a" and "b" to a and b constant variables
 
   return result;
 }
